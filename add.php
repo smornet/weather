d15821c620f0173ef76532ffaf42cf1a81e8d38e
Hugh Rawlinson <hugh@codeoclock.net>
2013-07-09 16:49:13 +0100

    Initial Commit

---
+++ add.php
@@ -0,0 +1,13 @@
+<?php
+
+$auth = $_GET['auth'];
+$temp = $_GET['temp'];
+$password = "12345";
+
+if($auth == $password){
+	file_put_contents("temp.txt", time().",".$temp."\n")
+}
+else{
+	echo "Authentication Failed";
+}
+?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+$file = "temp.txt";
+$data = file($file);
+$line = explode(",",$data[count($data)-1]);
+$time = $line[0];
+$temp = $line[1];
+
+?>
+<html>
+<head>
+	<title>Weather in Skibbereen</title>
+	<style>
+		p,h1,h2,h3,h4,h5,h6{
+			font-family: futura;
+			font-size: 300%;
+			color:#FFF;
+		}
+		
+		body{
+			background: #3b5998;
+		}
+		div{
+			width: 80%;
+			margin-left: auto;
+			margin-right: auto;
+		}
+	</style>
+</head>
+<body>
+	<div>
+		<h3>It is currently</h3>
+		<h1><?php
+		echo $temp;
+		?>°</h1>
+		<h3>in Skibbereen</h3>
+		<p>Most recently checked at <?php
+		echo $time;
+		?></p>
+	</div>
+</body>
+</html>
