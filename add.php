23eee876ab21dbcf0cb376944f334c498db1f58e
Hugh Rawlinson <hugh@codeoclock.net>
2013-08-05 16:38:50 +0100

    complete path

--- add.php
+++ add.php
@@ -5,10 +5,11 @@ $temp = $_GET['temp'];
 $password = "12345";
 
 if($auth == $password){
-	$file = "file.txt";
+	$file = "/var/www/weather/file.txt";
 	$f = fopen($file, 'a');
 	fwrite($f, date('l jS \of F Y h:i:s A').",".$temp.PHP_EOL);
 	fclose($f);
+	echo "Success";
 }
 else{
 	echo "Authentication Failed";
