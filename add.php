593e889b5663cda39584d06fe2081079c74d5b38
Hugh Rawlinson <hugh@codeoclock.net>
2013-08-05 16:10:18 +0100

    another n000b 3rr0r

--- add.php
+++ add.php
@@ -7,7 +7,7 @@ $password = "12345";
 if($auth == $password){
 	$file = "file.txt";
 	$f = fopen($file, 'a');
-	fwrite($f, date('l jS \of F Y h:i:s A');.",".$temp.PHP_EOL);
+	fwrite($f, date('l jS \of F Y h:i:s A').",".$temp.PHP_EOL);
 	fclose($f);
 }
 else{
