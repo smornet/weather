3d26aef61a67f350219c08a486b4af1170d01615
Hugh Rawlinson <hugh@codeoclock.net>
2013-08-05 16:43:10 +0100

    corrected file

--- add.php
+++ add.php
@@ -5,7 +5,7 @@ $temp = $_GET['temp'];
 $password = "12345";
 
 if($auth == $password){
-	$file = "/var/www/weather/file.txt";
+	$file = "/var/www/weather/temp.txt";
 	$f = fopen($file, 'a');
 	fwrite($f, date('l jS \of F Y h:i:s A').",".$temp.PHP_EOL);
 	fclose($f);
