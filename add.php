0f6fc54efc6a7c489274ecc78f0ea3bf21dc78b3
Hugh Rawlinson <hugh@codeoclock.net>
2013-08-05 16:02:04 +0100

    n0000b error fixed

--- add.php
+++ add.php
@@ -5,7 +5,7 @@ $temp = $_GET['temp'];
 $password = "12345";
 
 if($auth == $password){
-	file_put_contents("temp.txt", time().",".$temp."\n")
+	file_put_contents("temp.txt", time().",".$temp."\n");
 }
 else{
 	echo "Authentication Failed";
